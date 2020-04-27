7aa40d2a63b13ef1a4618d74008aed5e690a2e59
Gilles <cornec.gilles@gmail.com>
2020-04-27 17:31:18 +0200

    Adding test to check on nan values in the input dataset

--- Test/Tests.py
+++ Test/Tests.py
@@ -9,14 +9,19 @@ def test_columns_types(df):
     for col in ["pickup_latitude", "pickup_longitude", "dropoff_latitude", "dropoff_longitude"]:
         assert type(df.pickup_latitude.iloc[0]) == numpy.float64
 
+def test_nan(df):
+    assert df.isnull().sum().sum() == 0
+
 if __name__ == "__main__":
     import pandas as pd
     path = "Data/"
     filename = "train_sample.csv"
     df = pd.read_csv(path + filename, index_col = 0 )
-    print("1 - input lenght")
+    print("1 - input length equals 11")
     test_columns_len(df)
-    print("2 - coordinates types")
+    print("2 - coordinates types are floats")
     test_columns_types(df)
+    print("3 - no nan values")
+    test_nan(df)
 
     print("Everything passed")
